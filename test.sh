5ab822ca1e12b843ad1d34b55b925a0af3a7aee4
Andrea Falzetti <andrea@okteto.com>
2024-11-11 20:35:43 +0100

    update

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 OKTETO_USERNAME=andreafalzetti
 
 for i in {1..9}; do
-    # okteto ns delete $OKTETO_USERNAME-$i
+    okteto ns delete $OKTETO_USERNAME-$i
     # okteto ns create $OKTETO_USERNAME-$i
-    okteto deploy --namespace $OKTETO_USERNAME-$i --dependencies
+    # okteto deploy --namespace $OKTETO_USERNAME-$i --dependencies
 done
 
